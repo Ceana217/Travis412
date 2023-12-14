6c8f20bdf70d98397e8f6c60bc8683c36e87c477
Ceana217 <145236058+Ceana217@users.noreply.github.com>
2023-12-14 17:00:13 -0500

    2

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,6 +4,17 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
-	
-   
+	public int minus(int a, int b) {
+	    return a - b;
+	}	
+	public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+        return dividend / divisor;
+    }
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,5 +11,17 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
-	
+	public void testMinus() {
+	    SimpleCalculator calc = new SimpleCalculator();
+	    assertEquals(calc.minus(5, 3), 2);
+	}
+	public void testMultiply() {
+	    SimpleCalculator calc = new SimpleCalculator();
+	    assertEquals(calc.multiply(4, 3), 12);
+	}
+	public void testDivide() {
+	    SimpleCalculator calc = new SimpleCalculator();
+	    assertEquals(calc.divide(10, 2), 5);
+	}
 }
+
